0ce684b0562c9d5ecee5d534b4789646344c29ca
Kevin Hoogerwerf <kevinhoogerwerf@Kevins-MacBook-Pro.local>
2017-05-08 11:58:52 +0200

    changed package

--- src/sizes.ts
+++ src/sizes.ts
@@ -1,6 +1,6 @@
-
 import {RetrieveGlyphSet} from "./cache";
 import {CacheGlyphSet, RetrieveGlyphSize, AddNewGlyphToCache} from "./index";
+
 const BasicGlyphSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const UppercaseBasicGlyphSet = BasicGlyphSet.map((glyph) => glyph.toUpperCase());
 const NumberGlyphSet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
